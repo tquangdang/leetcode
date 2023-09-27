150be1aff457d94c664d101d40c61050e64b4917
tquangdang <thanhquangdang2002@gmail.com>
2023-09-27 15:53:07 -0500

    Add Flood fill

---
+++ graph/depth-first search/floodFill.py
@@ -0,0 +1,27 @@
+"""
+Time complexity: O(n)
+Space complexity: O(n) - call stack
+Thought process:
+- Perform dfs on the targeted pixel to fill them.
+- The important part is to set the condition of the recursion call
+"""
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # Keep a visited set 
+        visited = set()
+        startingColor = image[sr][sc]
+        def dfs (row, col):
+            # Conditions that will stop the recursion
+            if row < 0 or row >= len(image) or col < 0 or col >= len(image[0]) or image[row][col] != startingColor or (row, col) in visited:
+                return
+            # Else, add the current coordinate to visited
+            visited.add((row, col))
+            # Fill it to the desired color
+            image[row][col] = color
+            # Keep filling to adjacent part of the matrix
+            dfs(row + 1, col)
+            dfs(row , col + 1)
+            dfs(row - 1, col)
+            dfs(row, col - 1)
+        dfs(sr, sc)
+        return image
--- graph/tableOfContent.md
+++ graph/tableOfContent.md
@@ -3,6 +3,12 @@
 This section contains solutions and algorithms to Leetcode problems related to graphs.
 ## Table of Contents
 542. [01 Matrix](#542-01-matrix)
+733. [Flood Fill](#733-flood-fill)
 ### 542. 01 Matrix
 - [Problem Link](https://leetcode.com/problems/01-matrix/description/)
 - [Solution](breadth-first%20search/01Matrix.py)
+
+### 733. Flood Fill
+- [Problem Link](https://leetcode.com/problems/flood-fill/)
+- [Solution](depth-first%search/floodFill.py)
+
