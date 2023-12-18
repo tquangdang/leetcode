143b39f6b6ef353695342c67105116c23afe7437
tquangdang <thanhquangdang2002@gmail.com>
2023-12-17 20:29:51 -0600

    Add Two Sum II - Input Array Is Sorted

--- array/tableOfContent.md
+++ array/tableOfContent.md
@@ -3,11 +3,16 @@
 This section contains solutions and algorithms to Leetcode problems related to array
 ## Table of Contents.
 - [Best Time to Buy and Sell Stock](#121-best-time-to-buy-and-sell-stock)
+- [Two Sum II - Input Array Is Sorted](#167-two-sum-ii---input-array-is-sorted)
 - [Product of Array Except Self](#238-product-of-array-except-self)
 
 ### 121. Best Time to Buy and Sell Stock
 - [Problem Link](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
 - [Solution](./bestTimeToBuyAndSellStock.py)
+
+### 167. Two Sum II - Input Array Is Sorted
+- [Problem Link](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/)
+- [Solution](./twoSumIISorted.py)
 
 ### 238. Product of Array Except Self
 - [Problem Link](https://leetcode.com/problems/product-of-array-except-self/)
---
+++ array/twoSumIISorted.py
@@ -0,0 +1,27 @@
+from typing import List 
+"""
+Time complexity: O(n)
+Space complexity: O(1) - only 2 pointers were created
+Thought process:
+- We can use two pointers method to control the current sum. Since the list is sorted, 
+  we can increase the sum by moving the start pointer forward, and decrease the sum 
+  by moving the end pointer backward
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Initialize 2 pointers at the beginning and end of the array
+        start, end = 0, len(numbers) - 1
+        
+        # Loop through the array
+        while start < end:
+            # Calculate the total
+            total = numbers[start] + numbers[end]
+            # Decrease the total if it's smaller than the target
+            if total < target:
+                start += 1
+            # Else, increase the total
+            elif total > target:
+                end -= 1
+            # If the target is found:
+            else:
+                return [start + 1, end + 1]
